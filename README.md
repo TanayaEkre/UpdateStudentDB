7d9ef2ebfc7cd12b80a31e2a45e02e48ef6a3bad
Tanaya Ekre <tanayaekre2012@gmail.com>
2026-01-08 05:08:19 +0530

    Task 4:

---
+++ README.md
@@ -0,0 +1,21 @@
+# Task 4 – Update & Alter Operations (Oracle 10g)
+
+## Project Overview
+In this task, I performed table modification and data update operations on the students table in Oracle 10g.
+
+## What I Did
+1. Renamed an existing column
+2. Added a new column to the table
+3. Updated student data
+4. Verified changes in database
+
+## SQL Commands Used
+
+1. Update data
+UPDATE students SET marks = 85 WHERE id = 1;
+
+
+## Learning Outcome
+
+1. Learned UPDATE command
+2. Understood how to modify table structure and update records in Oracle database
---
+++ src/UpdateStudent.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class UpdateStudent {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Roll no whose data id to be Updated: ");
+        int RollNo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter New Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter New Marks: ");
+        int marks = sc.nextInt();
+
+        sc.close();
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:XE",
+                    "system",
+                    "@T1u6e04"
+            );
+
+            PreparedStatement stmt =
+                    con.prepareStatement("UPDATE students SET name=?, marks=? WHERE RollNo=?");
+
+            stmt.setString(1, name);
+            stmt.setInt(2, marks);
+            stmt.setInt(3, RollNo);
+
+            int count = stmt.executeUpdate();
+            System.out.println(count + " row(s) updated successfully");
+
+            con.close();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
